521092727bc25713f36ce0c759bf732441943d25
Godfrey T R <godfreytr.prof@gmail.com>
2025-12-11 09:50:18 +0530

    Delete constants.ts

--- constants.ts
+++
@@ -1,4 +0,0 @@
-
-export const APP_NAME = "Smart LMS";
-export const TRADITIONAL_USER_AVATAR = "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png";
-export const APP_LOGO_AVATAR = "https://upload.wikimedia.org/wikipedia/commons/1/15/Logo-smartlms.jpg";
